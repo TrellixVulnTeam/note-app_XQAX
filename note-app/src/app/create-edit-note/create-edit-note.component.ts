0143f27c64374da53bbebbfc645a559cfe2b2862
cornechri <cornelia.christof@outlook.com>
2022-08-04 17:03:23 +0200

    Router

--- note-app/src/app/create-edit-note/create-edit-note.component.ts
+++ note-app/src/app/create-edit-note/create-edit-note.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-create-edit-note',
   templateUrl: './create-edit-note.component.html',
-  styleUrls: ['./create-edit-note.component.scss']
+  styleUrls: ['./create-edit-note.component.scss'],
 })
 export class CreateEditNoteComponent implements OnInit {
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
